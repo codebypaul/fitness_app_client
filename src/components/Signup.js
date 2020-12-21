6a5bde905b6eac68c6144294fffb24c0079d05c6
Paul Williams <paul.d.williamsxiv@gmail.com>
2020-12-21 14:42:08 -0500

    created front end auth

---
+++ src/components/Signup.js
@@ -0,0 +1,96 @@
+// Imports
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+// import keys from '../utils/credentials';
+// const { REACT_APP_SERVER_URL } = keys;
+
+const Signup = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [DOB, setDOB] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handleDOB = (e) => {
+        setDOB(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (password === confirmPassword) {
+            const newUser = { firstName, lastName, email, DOB, password };
+            
+            axios.post(`${REACT_APP_SERVER_URL}/api/users/register`, newUser)
+            .then(response => {
+                console.log(response);
+                setRedirect(true);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }
+
+    if (redirect) return <Redirect to='/login' />
+    return (
+        <div className="row my-4 pb-5">
+            <div className="col-md-7 offset-md-3 pb-5">
+                <div className="card card-body my-3">
+                    <h2 className="py-2">Signup</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="name">First Name</label>
+                            <input type="text" name="First Name" value={firstName} onChange={handleFirstName} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Last Name</label>
+                            <input type="text" name="Last Name" value={lastName} onChange={handleLastName} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Date of Birth</label>
+                            <input type="date" name="dob" value={DOB} onChange={handleDOB} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control" />
+                        </div>
+                        <button type="submit" className="btn btn-primary float-right">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
