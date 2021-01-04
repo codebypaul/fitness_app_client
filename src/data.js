67359aa9a8618a2171ab641c2ab4a72d947901d4
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-01-04 13:43:54 -0800

    [data.js] created file to hold data to use dynamically

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = {
+    bodyParts: [
+        {
+            _id: '1',
+            category: 'Arms',
+            image: 'https://images.unsplash.com/photo-1590507621108-433608c97823?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80g'
+        },
+        
+        {
+            _id: '2',
+            category: 'Back',
+            image: 'https://images.unsplash.com/photo-1534367610401-9f5ed68180aa?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2100&q=80'
+        },
+
+        {
+            _id: '1',
+            category: 'Chest',
+            image: 'https://images.unsplash.com/photo-1532384555668-bc0c32a17ffd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2100&q=80'
+        },
+
+        {
+            _id: '1',
+            category: 'Legs',
+            image: 'https://images.unsplash.com/photo-1508215885820-4585e56135c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80'
+        },
+
+    ],
+}
+
+
+export default data;
